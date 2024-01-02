c4d55494c4fa54937f9452cf3d0fa7bd66e40af7
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-01-02 19:27:37 +0530

    Update call.py

--- sashix/core/call.py
+++ sashix/core/call.py
@@ -19,9 +19,9 @@ from pytgcalls.types.stream import StreamAudioEnded
 
 import config
 from strings import get_string
-from AnonX import LOGGER, YouTube, app
-from AnonX.misc import db
-from AnonX.utils.database import (add_active_chat,
+from sashix import LOGGER, YouTube, app
+from sashix.misc import db
+from sashix.utils.database import (add_active_chat,
                                        add_active_video_chat,
                                        get_assistant,
                                        get_audio_bitrate, get_lang,
@@ -30,11 +30,11 @@ from AnonX.utils.database import (add_active_chat,
                                        music_on, set_loop,
                                        remove_active_chat,
                                        remove_active_video_chat)
-from AnonX.utils.exceptions import AssistantErr
-from AnonX.utils.inline.play import (stream_markup,
+from sashix.utils.exceptions import AssistantErr
+from sashix.utils.inline.play import (stream_markup,
                                           telegram_markup)
-from AnonX.utils.stream.autoclear import auto_clean
-from AnonX.utils.thumbnails import gen_thumb
+from sashix.utils.stream.autoclear import auto_clean
+from sashix.utils.thumbnails import gen_thumb
 
 autoend = {}
 counter = {}
